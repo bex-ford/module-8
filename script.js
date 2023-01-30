2411952822bcc68f2d4f9ca13606baac2f2ecebc
bex-ford <becky782@hotmail.co.uk>
2023-01-30 21:11:57 +0000

    in progress

--- script.js
+++ script.js
@@ -1,32 +1,70 @@
-var APIKey = "51428ecdc304ebd2414d4b687653181a";
-// var queryURL = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}" + userInput + "&appid=" + APIKey;
-var userInput = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=51428ecdc304ebd2414d4b687653181a";
+const APIKey = "01870a3b381625539cc05bdbee086543"; // API key
+//moment.js
+const currentDate = moment().format("DD/M/YYYY");
+let searchForm = document.querySelector("#search-form");
+let searchInput = document.querySelector("#search-input");
+let searchBtn = document.querySelector("#search-button");
+let city = searchInput.value;
 
-// We then created an AJAX call
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-  // Create CODE HERE to Log the queryURL
-  console.log(queryURL);
-  // Create CODE HERE to log the resulting object
-  console.log(response);
-});
+function handleSearchForm(e) {
+  if (!searchInput.value) {
+    return;
+  }
+  e.preventDefault();
+  let city = searchInput.value.trim();
+  console.log(city);
+  searchWeather(city);
+}
+function searchWeather(city) {
+  let queryURL =
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=" +
+    APIKey;
 
+  fetch(queryURL)
+    .then((response) => {
+      console.log(response);
+      if (!response.ok) {
+        document.body.append("error getting coords");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      return data;
+    })
+    .catch((err) => console.log(err));
+}
 
+function currentWeather(search) {
+  let city = getCoords(search);
+  let lat = getCoords(city);
+  let lon = getCoords(city);
+  console.log(lat, lon, city);
+  console.log(city);
 
-//   // Create CODE HERE to calculate the temperature (converted from Kelvin)
-//   var celsiusTemperature = Math.floor(response.main.temp - 273.15);
-//   // Create CODE HERE to transfer content to HTML
-//   var myTempDiv = $('.temp');
-//   myTempDiv.text(celsiusTemperature + " degrees Celsius");
-//   var city = response.name;
-//   var humidity = response.main.humidity;
-//   var wind = response.wind.speed;
-//   $('.city').text("Weather for " + city);
-//   $('.humidity').text("Relative humidity: " + humidity + "%");
-//   $('.wind').text("Wind speed: " + wind + " m/s");
-//   // Hint: To convert from Kelvin to Celsius: C = K - 273.15
-//   // Create CODE HERE to dump the temperature content into HTML
+  let queryURL =
+    "http://api.openweathermap.org/data/2.5/weather?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&appid=" +
+    APIKey;
 
-// });
+  fetch(queryURL)
+    .then((response) => {
+      console.log(response);
+      if (!response.ok) {
+        console.log("response is not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      return data;
+    })
+    .catch((err) => console.log(err));
+  currentWeather("");
+}
+searchForm.addEventListener("submit", handleSearchForm);
