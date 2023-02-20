28bd739c413f68f674f168f2e2c1b21f0402c293
bex-ford <becky782@hotmail.co.uk>
2023-02-20 07:59:45 +0000

    API key working for current date

--- script.js
+++ script.js
@@ -1,49 +1,56 @@
-const APIKey = "01870a3b381625539cc05bdbee086543"; // API key
-//moment.js
-const currentDate = moment().format("DD/M/YYYY");
-let searchForm = document.querySelector("#search-form");
-let searchInput = document.querySelector("#search-input");
-let searchBtn = document.querySelector("#search-button");
-let city = searchInput.value;
+// Assign API key to a variable
+let APIKey = "fe3209cca110c675761038ad84996ecc";
 
-function handleSearchForm(e) {
-  if (!searchInput.value) {
-    return;
-  }
+// Using moment.js to store date
+let currentDate = moment().format("DD/M/YYYY");
+
+// Store input values in variables
+let searchForm = document.getElementById("search-form");
+let searchInput = document.getElementById("search-input");
+let cityName = document.getElementById("current-city");
+let tempEl = document.getElementById("temperature");
+let windEl = document.getElementById("wind");
+let humidityEl = document.getElementById("humidity");
+
+// Store city as variable with the value of search input
+let city = "";
+
+// Function to handle search form
+const handleSubmit = (e) => {
   e.preventDefault();
-  let city = searchInput.value.trim();
-  console.log(city);
-  searchWeather(city);
+};
+
+// Function to get current weather
+function currentWeather(e) {
+  e.preventDefault();
+  const search = searchInput.value;
+  getCoords(search);
 }
-function searchWeather(city) {
+
+// Function to query API for weather
+function getCoords(city) {
   let queryURL =
-    "http://api.openweathermap.org/data/2.5/weather?q=" +
+    "http://api.openweathermap.org/geo/1.0/direct?q=" +
     city +
     "&appid=" +
     APIKey;
 
   fetch(queryURL)
     .then((response) => {
-      console.log(response);
-      if (!response.ok) {
-        document.body.append("error getting coords");
-      }
       return response.json();
     })
     .then((data) => {
-      console.log(data);
-      return data;
+      // console.log(data);
+      const lat = data[0].lat;
+      const lon = data[0].lon;
+      getWeather(lat, lon);
+      getForecast(lat, lon);
     })
     .catch((err) => console.log(err));
 }
 
-function currentWeather(search) {
-  let city = getCoords(search);
-  let lat = getCoords(city);
-  let lon = getCoords(city);
-  console.log(lat, lon, city);
-  console.log(city);
-
+function getWeather(lat, lon) {
+  // Build API based on inputs
   let queryURL =
     "http://api.openweathermap.org/data/2.5/weather?lat=" +
     lat +
@@ -54,17 +61,42 @@ function currentWeather(search) {
 
   fetch(queryURL)
     .then((response) => {
-      console.log(response);
-      if (!response.ok) {
-        console.log("response is not ok");
-      }
       return response.json();
     })
     .then((data) => {
-      console.log(data);
-      return data;
+      cityName.textContent = data.name;
+      //  let locationIcon = document.querySelector('.weather-icon');
+      //  const {icon} = data.weather[0].icon;
+      //  locationIcon.innerHTML = `<img src="http://openweathermap.org/img/w/${icon}.png">`;
+      tempEl.textContent = " " + data.main.temp;
+      windEl.textContent = " " + data.wind.speed;
+      humidityEl.textContent = " " + data.main.humidity;
     })
     .catch((err) => console.log(err));
-  currentWeather("");
 }
-searchForm.addEventListener("submit", handleSearchForm);
+
+function getForecast(lat, lon) {
+  let days = 5;
+  // Build API based on inputs
+  let queryURL =
+    "http://api.openweathermap.org/data/2.5/forecast?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&cnt" +
+    days +
+    "&appid=" +
+    APIKey;
+
+  fetch(queryURL)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      // use for loop
+      // append to ID in HTML to a 5 day forecast
+      cityName.textContent = data.name;
+    });
+}
+
+searchForm.addEventListener("submit", currentWeather);
